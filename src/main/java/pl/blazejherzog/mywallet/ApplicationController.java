1a22f0d0a29bc316fe04e0dea3fa1c9c4496f76f
Blazej Herzog <blazejherzog@gmail.com>
2022-05-18 21:10:12 +0200

    Next commit

--- src/main/java/pl/blazejherzog/mywallet/ApplicationController.java
+++ src/main/java/pl/blazejherzog/mywallet/ApplicationController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.blazejherzog.mywallet.model.BudgetedAmount;
 import pl.blazejherzog.mywallet.model.Transaction;
 import pl.blazejherzog.mywallet.repositories.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -51,5 +53,39 @@ public class ApplicationController {
 
         }
         return ResponseEntity.ok(totalAmount);
+    }
+
+    @GetMapping("/month/{transactionsDate}/categories/{categoryName}/expenses")
+    public ResponseEntity getMonthlyExpensesByCategory(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate transactionsDate, @PathVariable String categoryName) {
+        List<Transaction> allTransactionsByMonthAndCategory = transactionRepository.findAll().stream()
+                .filter(transaction -> transaction.getDate().getMonth().equals(transactionsDate.getMonth()))
+                .filter(transaction -> transaction.getSubcategory().getCategory().getCategoryName().equals(categoryName))
+                .collect(Collectors.toList());
+        int totalAmount = 0;
+        for (Transaction transaction : allTransactionsByMonthAndCategory) {
+            int amount = transaction.getAmount();
+            totalAmount += amount;
+        }
+        return ResponseEntity.ok(totalAmount);
+    }
+
+    @GetMapping("/month/{transactionsDate}/categories/{categoryName}/budgetedamounts/left")
+    public ResponseEntity getAmountStillToSpend(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate transactionsDate, @PathVariable String categoryName) {
+        List<Transaction> allTransactionsByMonthAndCategory = transactionRepository.findAll().stream()
+                .filter(transaction -> transaction.getDate().getMonth().equals(transactionsDate.getMonth()))
+                .filter(transaction -> transaction.getSubcategory().getCategory().getCategoryName().equals(categoryName))
+                .collect(Collectors.toList());
+        int totalAmount = 0;
+        for (Transaction transaction : allTransactionsByMonthAndCategory) {
+            int amount = transaction.getAmount();
+            totalAmount += amount;
+        }
+        Optional<BudgetedAmount> monthlyBudgetedAmountFromDb = budgetedAmountRepository.findAll().stream()
+                .filter(budgetedAmount -> budgetedAmount.getBudgetedDate().getMonth().equals(transactionsDate.getMonth()))
+                .filter(budgetedAmount -> budgetedAmount.getCategory().getCategoryName().equals(categoryName))
+                .findFirst();
+
+        int amountStillToSpend = monthlyBudgetedAmountFromDb.get().getBudgetedAmount() - totalAmount;
+        return ResponseEntity.ok(amountStillToSpend);
     }
 }
