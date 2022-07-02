f61c0c5c42f6bfe8a2a524c885c067d02bce9e2d
Blazej Herzog <blazejherzog@gmail.com>
2022-07-02 20:54:25 +0200

    Next commit

--- src/main/java/pl/blazejherzog/mywallet/repositories/BudgetedAmountRepository.java
+++ src/main/java/pl/blazejherzog/mywallet/repositories/BudgetedAmountRepository.java
@@ -3,5 +3,7 @@ package pl.blazejherzog.mywallet.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.blazejherzog.mywallet.model.BudgetedAmount;
 
+import java.util.Optional;
+
 public interface BudgetedAmountRepository extends JpaRepository<BudgetedAmount, Integer> {
 }
